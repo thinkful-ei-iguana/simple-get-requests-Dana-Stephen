d5a55d980a56bffdf35b4a0b61d28a360f50cda8
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-11-01 12:50:23 -0700

    doggo error fixed, error function added

--- step-3/index.js
+++ step-3/index.js
@@ -2,22 +2,32 @@ const getBreed = function () {
   $('.doggoRanch').on('submit', event => {
     event.preventDefault();
     let breed = $('.doggo-breed').val();
+    console.log(breed);
     return getDoggos(breed);
   });
 };
 
 const displayResults = function(data) {
-  $('.photo-container').html  (`<img src="${data} alt="specific   doggo">`)
+  if(data.status==='error'){
+    $('.photo-container').html(`<p>${data.code}: ${data.message}</p>`);
+  }
+  else{
+    $('.photo-container').html(`<img src="${data}" alt="specific doggo">`);
+  }
 };
 
 const getDoggos = function (breed) {
   const options = {method: 'GET'};
+  console.log(breed);
   let callDoggo = `https://dog.ceo/api/breed/${breed}/images/random`;
+  console.log(callDoggo);
   fetch(callDoggo, options)
-    .then(response => response.json)
-    .then(jsonData => displayResults(jsonData)
-    )
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then(response => response.json())
+    .then(jsonData => {
+      displayResults(jsonData);
+      console.log(jsonData);
+    }
+    ).catch(error => alert('Something went wrong. Try again later.'));
 };
 
 $(getBreed());
