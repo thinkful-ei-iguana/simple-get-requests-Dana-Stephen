8ebd810e3e401ea78c468286273f0e8387bd794f
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-11-01 11:25:22 -0700

    printed doggos to console and dom

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 'use strict';
 
 const numberOfPics = function () {
-  $('user-input').on('click', event => {
-    let num = $('input').val();
-    return num;
+  $('.doggoRanch').on('submit', event => {
+    event.preventDefault();
+    console.log(event.currentTarget);
+    let num = $('.doggo-num').val();
+    console.log(num);
+    return getDoggos(num);
   });
 };
 
+const wrapDoggos = function (doggo) {
+  return `
+  <img
+    src= '${doggo}'
+    alt= 'doggo pic'
+    height= '100'
+    width= '100'
+  >
+  `;
+};
+
 const getDoggos = function (num) {
-  
-  fetch(https://dog.ceo/api/breeds/image/random/45)
-}
+  const options = {method: 'GET'};
+  const callDoggo = `https://dog.ceo/api/breeds/image/random/${num}`;
+  console.log(callDoggo);
+  fetch(callDoggo,options)
+    .then(response => response.json()).then( jsonData => {
+      const doggos = jsonData.message;
+      console.log(doggos);
+      let doggoHTML = doggos.map(doggo=> wrapDoggos(doggo)).join('');
+      $('.photo-container').html(doggoHTML);
+    }
+    );
+};
+
+
+
+console.log('hello');
+
+$(numberOfPics());
