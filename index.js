8b345cc658ab6fef901ba5172e51940b9687cb86
stephenschab <sschab689@gmail.com>
2019-11-01 10:14:26 -0600

    added number of pics function and started get doggos

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const numberOfPics = function () {
+  $('user-input').on('click', event => {
+    let num = $('input').val();
+    return num;
+  });
+};
+
+const getDoggos = function (num) {
+  
+  fetch(https://dog.ceo/api/breeds/image/random/45)
+}
